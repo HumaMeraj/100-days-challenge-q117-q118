493afce42fff3ca7e1731626b52d03f8b8e199aa
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-19 00:35:13 +0500

    File of q117

---
+++ q117.ts
@@ -0,0 +1,28 @@
+//Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+
+// This function evaluates a grade and logs the corresponding remark
+  function evaluateGrade(grade: string): void{
+        switch (grade){
+        case "A":
+        console.log("Execellent");
+        break;
+        case "B":
+        console.log("Good");
+        break;
+        case "C":
+        console.log("Fair");
+        break;
+        case "D":
+         console.log("Poor");
+         break; 
+         case "E":
+         console.log("Fail");
+         break;
+         default:
+         console.log("Invalid Grade");
+         break;                  
+    }
+  }
+  evaluateGrade("A");                      // Outputs: Execellent
+
+// Uses a switch statement to evaluate the grade and defaults to "Invalid grade" if no match is found.
