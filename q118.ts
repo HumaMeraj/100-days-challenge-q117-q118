9128e43e4e42f49fb9b116c73b74e7f4508e48a2
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-19 00:35:44 +0500

    File of q118

---
+++ q118.ts
@@ -0,0 +1,8 @@
+//Write a loop that logs numbers from 1 to 10 to the console.
+
+// This loop logs numbers from 1 to 10
+for (let i =1; i <= 10; i++) {
+    console.log(i);                            // Logs the current value of i
+}
+
+  // Each iteration increases the value of i by 1, logging it until it reaches 10.
